80e7808528381d1fb09e3f3441b44e66f894c7a1
Lawrence <locaulker@gmail.com>
2021-03-10 06:58:15 -0500

    end of section 4

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -8,7 +8,7 @@ const person = (props) => {
         I'm a {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
-      <input type="text" />
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   )
 }
